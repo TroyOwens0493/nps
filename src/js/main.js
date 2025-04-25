f1a371d0fec639d1710bb7a9078f0e301099a395
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2025-04-25 15:47:31 -0600

    finish week1 assignment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector('.disclaimer > a');
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+const parkInfoTemplate = (info) => {
+    return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
+
+const heroBanner = document.querySelector('.hero-banner__title');
+heroBanner.innerHTML = parkInfoTemplate(parkData);
+
+const title = document.querySelector('head > title');
+title.innerHTML = parkData.fullName;
+
+const heroImageElement = document.querySelector('.hero-banner > img');
+const heroImageUrl = parkData.images[0].url;
+heroImageElement.src = heroImageUrl;
