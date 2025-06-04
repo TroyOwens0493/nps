1babedf3bc961266a043a616db206190c815f4ee
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2025-06-04 13:10:56 -0600

    finish part 5

--- src/js/main.js
+++ src/js/main.js
@@ -24,4 +24,29 @@ async function init() {
     setParkInfoLinks(links);
 }
 
+function enableNavigation() {
+    const menuButton = document.querySelector("#global-nav-toggle");
+    const globalNav = document.querySelector(".global-nav__toggle");
+    console.log(globalNav);
+    // when the main menu button is clicked:
+    menuButton.addEventListener("click", (ev) => {
+        let target = ev.target;
+        // toggle the show class on the global-nav
+        globalNav.classList.toggle("show");
+        // check to see if ev.target is the button or something inside the button
+        if (target.tagName != "BUTTON") {
+            target = target.closest("button");
+        }
+        // check to see if we just opened or closed the menu
+        if (globalNav.classList.contains("show")) {
+            // if we opened it then set the aria-expanded attribute to true
+            target.setAttribute("aria-expanded", true);
+        } else {
+            // if we closed it then set the aria-expanded attribute to false
+            target.setAttribute("aria-expanded", false);
+        }
+    });
+}
+
 init();
+enableNavigation();
